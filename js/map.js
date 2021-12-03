d4dbfd7b90dabf6698c4efd52fceddb740c58cdd
Martin Schiegl <43884428+schmar00@users.noreply.github.com>
2021-12-03 08:30:21 +0100

    #

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 let USER_LANG = (navigator.language || navigator.language).substring(0, 2);
 let map = L.map('map').setView([47.7, 13.5], 7);
 
-$( "#loadOAI" ).click(function() {
+$("#loadOAI").click(function () {
   $('.loading').show();
   getData();
 });
@@ -17,7 +17,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
     USER_LANG = urlParams.get('lang');
   }
 
-
   createMap();
   //getData();
   mapData();
@@ -31,9 +30,9 @@ async function getData() {
   //let completeListSize = '';
   let allData = [];
 
-  for (i = 0; i < 20; i++) {
+  for (i = 0; i < 11; i++) {
     const contents = await $.getScript("load.php" + resumptionToken, function (data, textStatus, jqxhr) {
-      console.log(data);
+      //console.log(data);
       //remove xml part after </OAI-PMH>
       let parser = new DOMParser(),
         xmlDoc = parser.parseFromString(data.split('</OAI-PMH>')[0] + '</OAI-PMH>', 'text/xml');
@@ -63,8 +62,10 @@ async function getData() {
       body: `newData=${'const oaiArr = ' + JSON.stringify(allData) + ';'}`,
     })
     .then((response) => response.text())
-    .then((res) => {alert('OAI aktualisiert'); $('.loading').hide();});
-
+    .then((res) => {
+      alert('OAI aktualisiert');
+      $('.loading').hide();
+    });
 }
 
 
@@ -88,6 +89,8 @@ function createMap() {
 }
 
 function mapData() {
+  let title = '';
+  let creators = '';
 
   for (let i in oaiArr) {
 
@@ -99,59 +102,35 @@ function mapData() {
 
     let blattWidth = oaiArr[i].coverage[3] - oaiArr[i].coverage[1];
     //console.log(blattWidth, oaiArr[i].title, bounds);
+    title = $('<div>').text(oaiArr[i].title).html();
+    title = decodeHtml(title);
+    creators = $('<div>').text(oaiArr[i].creator.map(a=>a.replace(',','')).join(', ')).html();
+    creators = decodeHtml(creators);
 
     L.rectangle(bounds, {
       'color': (/GEOFAST/g).test(oaiArr[i].title) ? 'red' : 'green',
       'fill': (blattWidth > 0.3) ? false : true,
       'weight': (blattWidth > 0.3) ? 3 : 1
-    }).addTo(map).bindPopup(txt2html(`<a href="${oaiArr[i].doi}">${oaiArr[i].doi}</a>
-                                <p><strong>${oaiArr[i].title}</strong>
-                                  <br>publ.${oaiArr[i].date.substr(0, 4)} by ${oaiArr[i].creator.map(a=>a.replace(',','')).join(', ')}
-                                </p>`));
-    //console.log(txt2html(allData[i].title));
+    }).addTo(map).bindPopup(`<a href="${oaiArr[i].doi}">${oaiArr[i].doi}</a>
+                                <p><strong>${title}</strong>
+                                  <br>publ.${oaiArr[i].date.substr(0, 4)} by ${creators}
+                                </p>`);
+
   }
   let popup = L.popup();
   $('#loading').hide();
+
 }
 
-function txt2html(a) {
-  let arr = [
-    [
-      'Ä',
-      '&Auml;'
-    ],
-    [
-      'Ö',
-      '&Ouml;'
-    ],
-    [
-      'Ü',
-      '&Uuml;'
-    ],
-    [
-      'ä',
-      '&auml;'
-    ],
-    [
-      'ö',
-      '&ouml;'
-    ],
-    [
-      'ü',
-      '&uuml;'
-    ],
-    [
-      'ß',
-      '&szlig;'
-    ],
-    [
-      '–',
-      '&minus;'
-    ]
-  ];
-  for (let i of arr) {
-    a = a.replace(i[0], i[1]);
-  }
-  return a
-
+function decodeHtml(a) {
+  let char = {
+    '\\Ä': '&#196;',
+    '\\Ö': '&#214;',
+    '\\Ü': '&#220;',
+    '\\ä': '&#228;',
+    '\\ö': '&#246;',
+    '\\ü': '&#252;',
+    '\\ß': '&#223;'
+  };
+  return a.replace(/\\Ä|\\Ö|\\Ü|\\ä|\\ö|\\ü|\\ß/g, m => char[m]);
 }
--- php/update.php
+++ php/update.php
@@ -3,4 +3,3 @@ $json = $_POST['newData'];
 $file_pointer = 'tethys_oai.js';
 file_put_contents($file_pointer, $json);
 ?>
-
